ee357188a604f2d74fb13f55a744d504b9173d8b
priya2099 <haripriyaa2099@gmail.com>
2024-01-12 15:30:05 -0500

    changes

--- student.java
+++ student.java
@@ -3,11 +3,12 @@ package studyeasy;
 public class studyeasy {
 
 	public static void main(String[] args) {
-    
+		int x = 7;
+		
 		int y = 12;
 		short x = (byte)y;
 		System.out.println(y);
-		
+		System.out.println("1");
 		double v2 = 20.0123456789d;
 		float v1 = (float)v2;
 		System.out.println(v1);
