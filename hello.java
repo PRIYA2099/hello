a05ee19d845c672ce807457282cfff24d8992a63
priya2099 <haripriyaa2099@gmail.com>
2024-01-12 14:44:18 -0500

    add a new file

--- hello.java
+++ hello.java
@@ -2,6 +2,7 @@
 public class hello {
 
 	public static void main(String[] args) {
+		/*print*/
   System.out.println("Hello Happi!");
 	}
 
---
+++ student.java
@@ -0,0 +1,16 @@
+package studyeasy;
+
+public class studyeasy {
+
+	public static void main(String[] args) {
+    
+		int y = 10;
+		short x = (byte)y;
+		System.out.println(y);
+		
+		double v2 = 20.0123456789d;
+		float v1 = (float)v2;
+		System.out.println(v1);
+	}
+
+}
